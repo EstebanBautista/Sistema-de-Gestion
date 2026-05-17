ff0592661c282c0725d78f7427cff7c0f75ca403
Esteban Muñoz <0estebanmunoz0@gmail.com>
2026-05-17 13:33:12 -0500

    Configure production secret key base from environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"] if ENV["SECRET_KEY_BASE"].present?
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
